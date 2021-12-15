e529f9ef8b2d59020faed3ef027d041334f98d80
marcellojung <sungkun118@gmail.com>
2021-12-16 03:07:00 +0900

    상품API로직_1

---
+++ models/project.js
@@ -0,0 +1,27 @@
+//models/products.js
+//Article 테이블을 모델링하는 파일
+module.exports = function (sequelize, DataTypes) {
+  const product = sequelize.define("Product", {
+    name: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false,
+    },
+    seller: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING(300),
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.STRING(300),
+      allowNull: false,
+    },
+  });
+  return product;
+};
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const port = 8080;
+
+app.use(express.json()); //json 형식의 데이터를 처리할 수 있게 설정하는 코드
+app.use(cors()); //브라우저의 CORS 이슈를 막기 위해 사용하는 코드
+
+app.get("/products", async (req, res) => {
+  models.Product.findAll()
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("에러 발생");
+    });
+});
+
+app.post("/products", async (req, res) => {
+  const body = req.body; // {articles : [{...}]} 클라이언트가 보낸 json body가 출력됩니다.
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해 주세요!");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      //생성된 레코드 {url: ..., title, ...}를 반환합니다.
+      console.log("상품 생성결과", result);
+      res.send({ result });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 업로드에 문제가 발생했습니다.");
+    });
+});
+
+app.get("/products/:id", (req, res) => {
+  const params = req.params; //{id : 값} 형태로 들어옵니다.
+  const { id } = params; //ES6 Destructuring
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT :", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 조회에 에러가 발생했습니다.");
+    });
+});
+//세팅한 app을 실행시킨다.
+app.listen(port, () => {
+  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 에러");
+      process.exit();
+    });
+});
